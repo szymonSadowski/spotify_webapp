e9ae38ea7500e8418de092a619483c41b0e0532b
szymonSadowski <szymon.ssadows@gmail.com>
2021-02-12 11:14:43 +0100

    First commit add chakra ui and ts

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { Container } from "../components/Container";
+const Index = () => (
+  <Container height="100vh">
+    <DarkModeSwitch />
+  </Container>
+);
+
+export default Index;
